c193d2a1f110631156a119ad84f7b906ce450ed2
Ben <bskruger2002@gmail.com>
2024-07-08 16:48:48 +1000

    vim bindings

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -1,3 +1,29 @@
 -- Leader Keys
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+-- Navigate vim panes better
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+
+-- Escape highlights from search
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Disable arrow keys
+vim.keymap.set("n", "<left>", "<nop>")
+vim.keymap.set("n", "<right>", "<nop>")
+vim.keymap.set("n", "<up>", "<nop>")
+vim.keymap.set("n", "<down>", "<nop>")
+
+-- Half page centers
+vim.keymap.set("n", "<c-d>", "<c-d>zz")
+vim.keymap.set("n", "<c-u>", "<c-u>zz")
+
+-- Switch between files
+vim.keymap.set("n", "<leader><leader>", "<c-^>")
+
+-- Obsidian binds
+vim.keymap.set("n", "<leader>on", ":ObsidianTemplate note<cr> :lua vim.cmd([[1,/^\\S/s/^\\n\\{1,}//]])<cr>")
+vim.keymap.set("n", "<leader>of", ":s/\\(# \\)[^_]*_/\\1/ | s/-/ /g<cr>")
